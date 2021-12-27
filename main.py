502cf5f38cbc7693362e534ce6915a4162b6a154
Florin <florin.cuculeac@gmail.com>
2021-12-27 10:49:38 +0200

    update schema for create a post - structure of data

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+from typing import Optional
+
 from fastapi import FastAPI
-from fastapi.params import Body
+# from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+# define schema for the post
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True  # optional value
+    rating: Optional[int] = None
 
 
 @app.get("/")
@@ -14,7 +25,10 @@ def get_posts():
     return {"data": "this is your posts."}
 
 
+# validate structure for a new post -  title: str, content: str
 @app.post("/posts")
-def get_posts(payload: dict = Body(...)):
-    print(f"payload = {payload}")
-    return {"new_post": f"title={payload['title']}, content={payload['content']}"}
+def new_posts(new_post: Post):
+    print(f"payload = {new_post}")
+    print(f"payload as dict = {new_post.dict()}")
+    # return {"new_post": f"title={payload['title']}, content={payload['content']}"}
+    return {"data": new_post}
