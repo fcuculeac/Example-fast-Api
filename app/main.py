269c2d8fa0e7227807a840d3e4e1c8be7fbefffb
Florin <florin.cuculeac@gmail.com>
2022-01-07 08:42:40 +0200

    implement CORS (Cross-Origin Resource Sharing)

--- app/main.py
+++ app/main.py
@@ -1,13 +1,29 @@
 # run in CMD:  uvicorn.exe app.main:app --reload
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
 from .routers import posts, users, auth, votes
 from .config import Settings
 
-models.Base.metadata.create_all(bind=engine)
+
+# test conection from chrome
+# fetch("http://localhost:8000").then(res=>res.json()).then(console.log)
+
+# no need if alembic is implemented
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# allow all domains
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(posts.router)
 app.include_router(users.router)
